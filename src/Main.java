e8a4cac0631d098cdd08287b1fe3ef7f675b01ee
Nandhini M <nandhinimm4706@gmail.com>
2026-01-19 22:33:27 +0530

    methods return

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
-
+import Model.Login;
+import Model.User;
+import Model.Product;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
