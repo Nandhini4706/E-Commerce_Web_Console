41638b0a157a20587461fd7f34d2073d2750fe8c
Nandhini M <nandhinimm4706@gmail.com>
2026-01-21 23:06:11 +0530

    methods and class

--- src/Services/Billing.java
+++ src/Services/Billing.java
@@ -1,4 +1,13 @@
 package Services;
 
+import Model.Product;
+
 public class Billing {
+    private int billingId;
+    private int totalPrice;
+
+    public int calculatePrice(Product product, int quantity){
+        totalPrice= product.getProductPrice() * quantity;
+        return totalPrice;
+    }
 }
