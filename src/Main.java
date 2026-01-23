0840241a388257f464cb284c7dde09e150b57994
Nandhini M <nandhinimm4706@gmail.com>
2026-01-23 21:59:41 +0530

    main class

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,41 @@ import Model.User;
 import Model.Product;
 import Services.ProductService;
 public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        HashMap<Integer,Product> map=new HashMap<>();
-        ProductService ps=new ProductService();
-        map.put(1,new Product(1,"Samsung","Samsung mobile 5G", 120000));
-        map.put(2,new Product(2,"Vivo","Vivo mobile 5G", 150000));
-      while(true) {
-          System.out.println("Enter user Email: ");
-          String str = sc.nextLine();
-          String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-boolean boo=false;
-          if (str.matches(regex)) {
-              System.out.println("Login Successfully✅");
-              System.out.println("Enter Password");
-              String password = sc.nextLine();
-              String pass = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\n";
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Product> map = new HashMap<>();
+        ProductService ps = new ProductService();
+        map.put(1, new Product(1, "Samsung", "Samsung mobile 5G", 120000));
+        map.put(2, new Product(2, "Vivo", "Vivo mobile 5G", 150000));
+        while (true) {
+            System.out.println("Enter user Email: ");
+            String str = sc.nextLine();
+            boolean boo = "";
+            String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
-              if (password.matches(pass)) {
-                  System.out.println("Password Created Successfully");
-                  boo=true;
-                  continue;
-              } else {
-                  System.out.println("Password is weak");
-              }
-          } else {
-              System.out.println("Invalid Email");
-          }
-         if(boo==true){
-             System.out.println("Welcome to E-Commerce");
+            if (str.matches(regex)) {
+                System.out.println("Login Successfully✅");
+                System.out.println("Enter Password");
 
-         }
-      }
+                String password = sc.nextLine();
 
+                String pass = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+                if (password.matches(pass)) {
+                    System.out.println("Password Created Successfully");
+                    boo = true;
+                    continue;
+                } else {
+                    System.out.println("Password is weak");
+                }
+            } else {
+                System.out.println("Invalid Email");
+            }
+            if (boo == true) {
+                System.out.println("Welcome to E-Commerce");
+
+            }
+        }
+    }
     }
 }
