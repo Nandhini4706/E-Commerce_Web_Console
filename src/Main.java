a7c8c5c7aea79bb04e2eb49dbeec2973b56ea33d
Nandhini M <nandhinimm4706@gmail.com>
2026-01-25 23:33:44 +0530

    main class

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashMap<Integer, Product> map = new HashMap<>();
-        ProductService ps = new ProductService();
+      //  ProductService ps = new ProductService();
+
         map.put(1, new Product(1, "Samsung", "Samsung mobile 5G", 120000));
         map.put(2, new Product(2, "Vivo", "Vivo mobile 5G", 150000));
 
@@ -48,19 +49,34 @@ public class Main{
             }
             if(boo){
                 System.out.println("Welcome To E-commerce ");
+                System.out.println("1. Mobile Phones");
+                System.out.println("2. Furniture");
+                System.out.println("3. Cosmetic");
+                System.out.println("4. Dress");
                 System.out.println("-----------------------------------------------------------------------------------------------");
                 System.out.println("|Product S.No|   Product Name   |                   Description              |     Price     |");
                 System.out.println("-----------------------------------------------------------------------------------------------");
+              int num=sc.nextInt();
+                switch(num) {
+                    case 1:
+                    for (Product p : map.values()) {
 
-                for(Product p :map.values()){
+                        System.out.println("|      " + p.getProductId() + "     |       " + p.getProductName() + "    |               " + p.getProductDiscription() + "             |   " + p.getProductPrice() + "      |");
 
-                    System.out.println("|      " +p.getProductId() +"     |       " +p.getProductName() +"    |               " +p.getProductDiscription() +"             |   " +p.getProductPrice()+"      |");
+                    }
+                    System.out.println("-----------------------------------------------------------------------------------------------");
+                        break;
+                    case 2:
+                        map.put(1, new Product(201,"Refrigerator","Samsung",350000));
+                        for(Product p:map.values()){
+                            if(map.getKey()>=1){
+                                System.out.println("|      " + p.getProductId() + "     |       " + p.getProductName() + "    |               " + p.getProductDiscription() + "             |   " + p.getProductPrice() + "      |");
 
+                            }
+                        }
                 }
-                System.out.println("-----------------------------------------------------------------------------------------------");
 
 
-                break;
 
             }
         }
